e851ecf8d8d3fae73a60276a5ba5eecd641c43d1
Andrew Robinson. <andrew0robinson@yahoo.co.uk>
2023-12-30 12:36:42 +0000

    Created and styled the sidebar component

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,9 @@
+const NewProject = () => {
+  return (
+    <dialog className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md" open>
+      test
+    </dialog>
+  );
+};
+
+export default NewProject;
