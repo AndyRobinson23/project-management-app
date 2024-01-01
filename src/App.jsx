a0a853c8c1ee8e857a64babfce4078b32c6502bd
Andrew Robinson. <andrew0robinson@yahoo.co.uk>
2024-01-01 16:12:56 +0000

    added the ability to save a new project, which updates the projects data array

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,54 @@ import { useState } from 'react';
 
 import Sidebar from './components/Sidebar';
 import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
 
 function App() {
-  const [showNewProjectForm, setShowNewProjectForm] = useState(false);
+  const [projectsState, setProjectsState] = useState({
+    // The selectedProjectId is initially undefined but changes to null if a project is being added
+    selectedProjectId: undefined,
+    projects: [],
+  });
 
   const handleAddProjectClick = () => {
-    setShowNewProjectForm(true);
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
   };
-  const handleCloseClick = () => {
-    setShowNewProjectForm(false);
+
+  const handleAddProject = (projectData) => {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
   };
+
+  console.log(projectsState);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleAddProjectClick} />;
+  }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar addProjectClickHandler={handleAddProjectClick} />
-      {showNewProjectForm && (
-        <NewProject closeClickHandler={handleCloseClick} />
-      )}
+      <Sidebar
+        onStartAddProject={handleAddProjectClick}
+        projects={projectsState.projects}
+      />
+      {content}
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,43 +1,39 @@
+import { useRef } from 'react';
 import Input from './Input';
 
-const NewProject = ({ closeClickHandler }) => {
+const NewProject = ({ onAdd }) => {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  const handleSave = () => {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  };
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
+        <button className="text-stone-800 hover:text-stone-950">Close</button>
         <button
-          className="text-stone-800 hover:text-stone-950"
-          onClick={closeClickHandler}
+          className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+          onClick={handleSave}
         >
-          Close
-        </button>
-        <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
           Save
         </button>
       </menu>
-      <Input label="Title" type="text" />
-      <Input label="Description" type="text" />
-      <Input label="Due Date" type="date" />
-      {/* <p className="flex flex-col gap-1 my-4">
-        <label className="text-sm font-bold uppercase text-stone-500">
-          Title
-        </label>
-        <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
-      </p>
-      <p className="flex flex-col gap-1 my-4">
-        <label className="text-sm font-bold uppercase text-stone-500">
-          Decription
-        </label>
-        <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
-      </p>
-      <p className="flex flex-col gap-1 my-4">
-        <label className="text-sm font-bold uppercase text-stone-500">
-          Due Date
-        </label>
-        <input
-          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-          type="date"
-        />
-      </p> */}
+      <div>
+        <Input label="Title" type="text" ref={title} />
+        <Input label="Description" textarea ref={description} />
+        <Input label="Due Date" type="date" ref={dueDate} />
+      </div>
     </div>
   );
 };
